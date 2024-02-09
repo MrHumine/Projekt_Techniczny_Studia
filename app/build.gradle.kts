c77f3cecc98b9c28b7509ad13042406a7720db83
MrHumine <pubh@interia.pl>
2024-02-09 14:58:36 +0000

    Adding creating_new_profile_of_friends.xml Adding list_of_firends_intent.xml Settings example layouts for activity_main.xml, creating_new_profile_of_friends.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.projekt_techniczny_studia"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.projekt_techniczny_studia"
