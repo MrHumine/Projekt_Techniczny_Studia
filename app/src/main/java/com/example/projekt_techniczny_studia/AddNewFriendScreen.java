dff7d989d4fee2a326df03b31db1381a9bc13284
MrHumine <pubh@interia.pl>
2024-06-20 11:06:28 +0200

    Dodano ScrollView w tworzeniu nowego profilu

--- app/src/main/java/com/example/projekt_techniczny_studia/AddNewFriendScreen.java
+++ app/src/main/java/com/example/projekt_techniczny_studia/AddNewFriendScreen.java
@@ -13,8 +13,15 @@ public class AddNewFriendScreen extends AppCompatActivity {
     Button buttonClose;
     Button buttonAdd;
     TextInputEditText textInputEditTextName;
+    TextInputEditText textInputEditTextLocalization;
+    TextInputEditText textInputEditTextHairColour;
+    TextInputEditText textInputEditTextEyesColour;
+    TextInputEditText textInputEditTextDescription;
     TextView textViewName;
     TextView textViewLocalization;
+    TextView textViewHairColour;
+    TextView textViewEyesColour;
+    TextView textViewDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +30,19 @@ public class AddNewFriendScreen extends AppCompatActivity {
 
         buttonClose = findViewById(R.id.button_close_of_creating_new_profile_of_friend);
         buttonAdd = findViewById(R.id.button_add_description_creating_new_profile);
+
         textInputEditTextName = findViewById(R.id.text_input_add_name);
+        textInputEditTextLocalization =findViewById(R.id.text_input_add_localization);
+        textInputEditTextHairColour = findViewById(R.id.text_input_add_hair);
+        textInputEditTextEyesColour = findViewById(R.id.text_input_add_eyes);
+        textInputEditTextDescription = findViewById(R.id.text_input_add_description);
+
         textViewName = findViewById(R.id.textViewName);
         textViewLocalization = findViewById(R.id.text_view_localization);
+        textViewHairColour = findViewById(R.id.textViewHairColour);
+        textViewEyesColour = findViewById(R.id.textViewEyesColour);
+        textViewDescription = findViewById(R.id.textViewDescription);
+
 
         buttonClose.setOnClickListener((view) ->{
             finish();
@@ -34,6 +51,14 @@ public class AddNewFriendScreen extends AppCompatActivity {
         buttonAdd.setOnClickListener((view) ->{
             String name = String.valueOf(textInputEditTextName.getText());
             textViewName.setText(name);
+            String hair = String.valueOf(textInputEditTextHairColour.getText());
+            textViewHairColour.setText(hair);
+            String eyes = String.valueOf(textInputEditTextEyesColour.getText());
+            textViewEyesColour.setText(eyes);
+            String localization = String.valueOf(textInputEditTextLocalization.getText());
+            textViewLocalization.setText(localization);
+            String description = String.valueOf(textInputEditTextDescription.getText());
+            textViewDescription.setText(description);
         });
     }
 }
